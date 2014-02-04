ea6ac5fa58d1ba52d6092d4ac0c0a1703abc6450
Freddy Kaiser <freddy.kaiser@swisscom.com>
2014-02-04 09:32:54 +0100

    Splitting certs into dedicated files refs #47
    Remains the TMP vs $TMP issue

--- bash/allin-verify.sh
+++ bash/allin-verify.sh
@@ -3,7 +3,7 @@
 #
 # Script to verify the integrity of a file with a detached PKCS#7 signature.
 #
-# Dependencies: openssl, sed
+# Dependencies: openssl, sed, awk
 #
 # License: GNU General Public License version 3 or later; see LICENSE.md
 # Author: Swisscom (Schweiz) AG
@@ -41,7 +41,7 @@ fi
 PWD=$(dirname $0)                               # Get the Path of the script
 
 # Check the dependencies
-for cmd in openssl sed; do
+for cmd in openssl sed awk; do
   hash $cmd &> /dev/null
   if [ $? -eq 1 ]; then error "Dependency error: '$cmd' not found" ; fi
 done
@@ -65,23 +65,29 @@ openssl smime -verify -inform pem -in $SIG -content $FILE -out $TMP.sig -CAfile 
 
 RC=$?                                           # Keep the related errorlevel
 if [ "$RC" = "0" ]; then                        # Verification ok
-  # Extract the signers certificate
-  openssl pkcs7 -inform pem -in $SIG -out $TMP.certificates.pem -print_certs > /dev/null 2>&1
-  [ -s "${TMP}.certificates.pem" ] || error "Unable to extract signers certificate from signature"
-  RES_CERT_SUBJ=$(openssl x509 -subject -nameopt utf8 -nameopt sep_comma_plus -noout -in $TMP.certificates.pem)
-  RES_CERT_ISSUER=$(openssl x509 -issuer -nameopt utf8 -nameopt sep_comma_plus -noout -in $TMP.certificates.pem)
-  RES_CERT_START=$(openssl x509 -startdate -noout -in $TMP.certificates.pem)
-  RES_CERT_END=$(openssl x509 -enddate -noout -in $TMP.certificates.pem)
+  # Extract the certificates in the signature
+  openssl pkcs7 -inform pem -in $SIG -out $TMP.certs.pem -print_certs > /dev/null 2>&1
+  [ -s "${TMP}.certs.pem" ] || error "Unable to extract the certificates in the signature"
+  # Split the certificate list into separate files
+  cat $TMP.certs.pem | awk -v c=-1 '/-----BEGIN CERTIFICATE-----/{inc=1;c++}
+                                    inc {print > ("TMP.certs.level" c ".pem")}
+                                    /---END CERTIFICATE-----/{inc=0}'
+  # Signers certificate is in level0
+  [ -s "TMP.certs.level0.pem" ] || error "Unable to extract signers certificate from the list"
+  RES_CERT_SUBJ=$(openssl x509 -subject -nameopt utf8 -nameopt sep_comma_plus -noout -in TMP.certs.level0.pem)
+  RES_CERT_ISSUER=$(openssl x509 -issuer -nameopt utf8 -nameopt sep_comma_plus -noout -in TMP.certs.level0.pem)
+  RES_CERT_START=$(openssl x509 -startdate -noout -in TMP.certs.level0.pem)
+  RES_CERT_END=$(openssl x509 -enddate -noout -in TMP.certs.level0.pem)
 
   # Get OCSP uri from the signers certificate and verify the revocation status
-  OCSP_URL=$(openssl x509 -in $TMP.certificates.pem -ocsp_uri -noout)
+  OCSP_URL=$(openssl x509 -in TMP.certs.level0.pem -ocsp_uri -noout)
 
   # Verify the revocation status over OCSP
   if [ -n "$OCSP_URL" ]; then
-    openssl ocsp -CAfile $SIG_CA -issuer $SIG_CA -nonce -out $TMP.certificates.check -url $OCSP_URL -cert $TMP.certificates.pem > /dev/null 2>&1
+    openssl ocsp -CAfile $SIG_CA -issuer TMP.certs.level1.pem -nonce -out $TMP.certs.check -url $OCSP_URL -cert TMP.certs.level0.pem > /dev/null 2>&1
     OCSP_ERR=$?                                   # Keep related errorlevel
     if [ "$OCSP_ERR" = "0" ]; then                # Revocation check completed
-      RES_CERT_STATUS=$(sed -n -e 's/.*.certificates.pem: //p' $TMP.certificates.check)
+      RES_CERT_STATUS=$(sed -n -e 's/.*.certs.level0.pem: //p' $TMP.certs.check)
      else                                         # -> check not ok
       RES_CERT_STATUS="error, status $OCSP_ERR"    # Details for OCSP verification
     fi
@@ -106,8 +112,9 @@ fi
 # Cleanups if not DEBUG mode
 if [ ! -n "$DEBUG" ]; then
   [ -f "$TMP" ] && rm $TMP
-  [ -f "$TMP.certificates.pem" ] && rm $TMP.certificates.pem
-  [ -f "$TMP.certificates.check" ] && rm $TMP.certificates.check
+  [ -f "$TMP.certs.pem" ] && rm $TMP.certs.pem
+  ## TODO REMOVE ALL LEVEL certs
+  [ -f "$TMP.certs.check" ] && rm $TMP.certs.check
   [ -f "$TMP.sig" ] && rm $TMP.sig
   [ -f "$TMP.verify" ] && rm $TMP.verify
 fi
